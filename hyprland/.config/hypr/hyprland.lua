3db9877129f8689605103f9657ec9038e648ed08
Marcos Ojeda <193316448+SrCiocco@users.noreply.github.com>
2026-08-13 21:53:00 -0300

    add(hyprland): add qt6ct env

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -70,7 +70,7 @@ end)
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 -----------------------
 ----- PERMISSIONS -----
 -----------------------
